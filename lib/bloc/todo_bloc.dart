2906b01539bd667b231f74fc34d315ea4091440b
emzhran <zachran750@gmail.com>
2025-05-08 13:15:21 +0700

    membuat todo bloc dan menambahkan event TodoEventAdd untuk menambah todo

---
+++ lib/bloc/todo_bloc.dart
@@ -0,0 +1,28 @@
+import 'package:bloc/bloc.dart';
+import 'package:bloc_118/model/todo.dart';
+import 'package:meta/meta.dart';
+
+part 'todo_event.dart';
+part 'todo_state.dart';
+
+class TodoBloc extends Bloc<TodoEvent, TodoState> {
+  TodoBloc() : super(TodoLoaded(todos: [], selectedDate: null)) {
+    on<TodoEventAdd>((event, emit) {
+      final currentstate = state;
+      if (currentstate is TodoLoaded) {
+        final List<Todo> updateTodos = List.from(currentstate.todos);
+        updateTodos.add(
+          Todo(
+            title: event.title, 
+            date: event.date, 
+            isComplete: false)
+        );
+        emit(
+          TodoLoaded(
+            todos: updateTodos, 
+            selectedDate: currentstate.selectedDate)
+        );
+      }
+    });
+  }
+}
